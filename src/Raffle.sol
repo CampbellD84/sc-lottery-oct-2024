373864c42eb4d6698526698e2bcb318c2baccc99
Douglas Campbell <dacampbelltbone@gmail.com>
2024-10-20 01:11:28 -0400

    chore: forge init

---
+++ src/Raffle.sol
@@ -0,0 +1,88 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts@1.1.1/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+
+/**
+ * @title A sample Raffle contract
+ * @author Douglas Campbell
+ * @notice This contract is for creating sample Raffle
+ * @dev Implements Chainlink VRF v2.5
+ */
+contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle();
+
+    uint256 private immutable i_entranceFee;
+    // @dev Duration of Lottery in seconds
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    /* Events */
+    event RaffleEntered(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval) {
+        i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+    }
+
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not Enough ETH Sent!"); -- NOT Gas Efficient (Storing String)
+        // require(msg.value >= i_entranceFee, SentMoreToEnterRaffle()); -- NOT Gas Efficient, also NOT AVAILABLE with this solidity/compiler version
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+
+        s_players.push(payable(msg.sender));
+
+        emit RaffleEntered(msg.sender);
+    }
+
+    function pickWinner() external {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+
+        requestId = s_vrfCoordinator.requestRandomWords(
+            VRF2PlusClient.RandomWordsRequest({
+                keyHash: s_keyHash,
+                subId: s_subscriptionId,
+                requestConfirmations: requestConfirmations,
+                callbackGasLimit: callbackGasLimit,
+                numWords: numWords,
+                extraArgs: VRF2PlusClient._argsToBytes(VRF2PlusClient.ExtraArgsV1({nativePayment: false}))
+            })
+        );
+    }
+
+    /**
+     * Getter Functions
+     */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
